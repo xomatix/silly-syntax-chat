9b1b66529d2dde6ae3893d4f360770c4319e7b39
Mateusz Świerczek <omatix2003@interia.pl>
2024-10-17 23:20:05 +0200

    1 Login screen functionality c make it look more like rest of the app

--- src/login/Login.tsx
+++ src/login/Login.tsx
@@ -25,29 +25,34 @@ function Login() {
   };
 
   return (
-    <div>
-      <div className="max-w-sm mx-auto bg-gray-900 shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <div className="mb-5">
+    <div className="flex h-screen items-center justify-center bg-gray-100">
+      <div className="w-1/4 bg-white border border-gray-200 rounded-lg shadow-lg p-6">
+        <h2 className="text-xl font-semibold mb-6 text-gray-900 text-center">
+          Login
+        </h2>
+        {/* Login input */}
+        <div className="mb-4">
           <label
             htmlFor="email"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            className="block mb-2 text-sm font-medium text-gray-900"
           >
-            Login
+            Email
           </label>
           <input
             type="text"
             id="email"
             value={login}
             onChange={(e) => setLogin(e.target.value)}
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="name@flowbite.com"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            placeholder="name@example.com"
             required
           />
         </div>
-        <div className="mb-5">
+        {/* Password input */}
+        <div className="mb-6">
           <label
             htmlFor="password"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            className="block mb-2 text-sm font-medium text-gray-900"
           >
             Password
           </label>
@@ -56,30 +61,14 @@ function Login() {
             id="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
             required
           />
         </div>
-        {/* <div className="flex items-start mb-5">
-          <div className="flex items-center h-5">
-            <input
-              id="remember"
-              type="checkbox"
-              value=""
-              className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-              required
-            />
-          </div>
-          <label
-            htmlFor="remember"
-            className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-          >
-            Remember me
-          </label>
-        </div> */}
+        {/* Submit button */}
         <button
           onClick={handleLogin}
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          className="w-full bg-blue-600 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-300"
         >
           Submit
         </button>
